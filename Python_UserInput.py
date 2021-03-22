414b5519932d1d1d73380ff7729cbfd9c20b192d
AyPeeEatsDaal <p17195823@my365.dmu.ac.uk>
2021-03-22 16:03:31 +0000

    added and completed python user input

---
+++ Python_UserInput.py
@@ -0,0 +1,27 @@
+
+# User Input Task
+# Gather the input the user enters and return what data type it is
+
+first_name = input("What is your First Name?")
+print(first_name)
+print("This is a String")
+
+second_name = input("What is your Second Name?")
+print(second_name)
+print("This is also a sting")
+
+age = input("How Old Are You?")
+print(age)
+print("This is a Integer")
+
+salary = input("How much do you Earn?")
+print(salary)
+print("This is a float")
+
+dob = input("When was you born?")
+print(dob)
+print("This is a string")
+
+postC = input("What is Your Post Code?")
+print(postC)
+print("This is a string")
